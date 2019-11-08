794e8c4616a37e94b167072ec7dac212c45c2541
Loopshape <webscreen.designer@gmail.com>
2019-11-08 06:24:52 +0100

    init upload

---
+++ public/typo3conf/LocalConfiguration.php
@@ -0,0 +1,37 @@
+<?php
+return [
+    'BE' => [
+        'explicitADmode' => 'explicitAllow',
+        'loginSecurityLevel' => 'normal',
+    ],
+   'DB' => [
+       'Connections' => [
+           'Default' => [
+               'charset' => 'utf-8',
+               'dbname' => 't3',
+               'driver' => 'sqlsrv',
+               'host' => '127.0.0.1',                                                                                                                                                                                  'password' => '13676778777',                                                                                                                                                                            'port' => 1433,
+               'user' => 'root',                                                                                                                                                                                    ],
+        ],
+    ],
+    'EXTENSIONS' => [
+        'backend' => [
+            'backendFavicon' => '',
+            'backendLogo' => '',
+            'loginBackgroundImage' => '',
+            'loginFootnote' => '',
+            'loginHighlightColor' => '',
+            'loginLogo' => '',
+        ],
+        'extensionmanager' => [
+            'automaticInstallation' => '1',
+            'offlineMode' => '0',
+        ],
+    ],
+    'SYS' => [
+        'encryptionKey' => '5585f3312a3f9811e8e58533516400a05bd71629e304656af2a8ae2446c54e818d71111381db673458e80f877ce5d19e',
+        'features' => [
+            'unifiedPageTranslationHandling' => true,
+        ],
+        'sitename' => 'LOOPSHAPE',                                                                                                                                                                          ],
+];
